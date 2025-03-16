85ccfa08e28eefb912dc307e9872d732c04638de
abelmuro <abelmuro93@gmail.com>
2025-03-16 16:55:37 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -61,6 +61,8 @@ app.use(leaveQueue);
 app.use(createNewChallenge);
 
 app.get('/', (req, res) => {
+    res.send(__dirname);
+    return;
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
