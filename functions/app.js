a1910c70079e750ca746616497e9e93890b77fa0
AbelMuro <abelmuro93@gmail.com>
2025-03-18 16:50:05 -0700

    commit

---
+++ functions/app.js
@@ -0,0 +1,10 @@
+const serverless = require('serverless-http'); 
+const app = require('../src/index.js'); 		//make sure you export the app module from the index.js
+  const connectDB = require('../src/Database/db.js');	//if you are using mongoose
+
+const handler = serverless(app);  		       //you can use     module.exports.handler = handler       as well
+module.exports.handler = async (e, context) => {	//you can use a callback to connect to databases or some other async logic that must be implemented before every request
+      await connectDB();					// you will need to call the connectDB() everytime there is a request made by the front end
+    const result = await handler(e, context);
+    return result;
+};
