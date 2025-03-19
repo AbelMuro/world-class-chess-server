3eb553cc9177b96bd0e30b0a4d83b91a1a935400
AbelMuro <abelmuro93@gmail.com>
2025-03-18 17:01:43 -0700

    commit

--- functions/app.js
+++ functions/app.js
@@ -1,10 +1,25 @@
 const serverless = require('serverless-http'); 
-const app = require('../src/index.js'); 		//make sure you export the app module from the index.js
-  const connectDB = require('../src/Config/MongoDB/DB.js');	//if you are using mongoose
+const app = require('../src/index.js'); 		
+  const connectDB = require('../src/Config/MongoDB/DB.js');
 
-const handler = serverless(app);  		       //you can use     module.exports.handler = handler       as well
-module.exports.handler = async (e, context) => {	//you can use a callback to connect to databases or some other async logic that must be implemented before every request
-      await connectDB();					// you will need to call the connectDB() everytime there is a request made by the front end
+const handler = serverless(app);  		      
+module.exports.handler = async (e, context) => {	
+  await connectDB();		
+
+  if(e.path === '/get_messages'){
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+      },
+      body: "data: SSE connection established"
+    }
+  }
+  else{
     const result = await handler(e, context);
-    return result;
+    return result;    
+  }
+
 };
