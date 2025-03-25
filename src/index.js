612d226c4143a7bb26502a46b1464aeef1bddbf6
AbelMuro <abelmuro93@gmail.com>
2025-03-24 17:15:50 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ const connectDB = require('./Config/MongoDB/DB.js');
 
 const app = express();   
 const indexFilePath = path.join(__dirname, 'index.html');   
-const privateKeyFilePath = path.join(__dirname, '../HTTPS/private.key');
-const certificateFilePath = path.join(__dirname, '../HTTPS/certificate.cer'); 
+const privateKeyFilePath = path.join(__dirname, '../SSL/private.key');
+const certificateFilePath = path.join(__dirname, '../SSL/certificate.cer'); 
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
