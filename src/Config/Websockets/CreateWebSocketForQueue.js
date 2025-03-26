a53053ff7b795c551ae422ec7a7b82b7c0d2e4c9
AbelMuro <abelmuro93@gmail.com>
2025-03-26 14:20:11 -0700

    commit

--- src/Config/Websockets/CreateWebSocketForQueue.js
+++ src/Config/Websockets/CreateWebSocketForQueue.js
@@ -16,10 +16,18 @@ const CreateWebSocketForQueue = async () => {
 
     try{
         const changeStream = Queue.watch();
-
         const wss = new WebSocket.Server({server});
+        
+        server.on('upgrade', (request, socket, head) => {               // upgrade event will be triggered when the client sends a request to upgrade from http request to websocket request
+            if(request.headers['upgrade'] !== 'websocket')              // if the client is not requesting an upgrade to websocket, then we destroy the websocket
+                socket.destroy();
+            else
+                wss.handleUpgrade(request, socket, head, (ws) => {      // we handle the upgrade here
+                    wss.emit('connection', ws, request);                // we create the websocket connection
+                })
+        })
 
-        wss.on('connection', ws => {                                        //Third, you establish the connection between the back end and the front end
+        wss.on('connection', ws => {                                    //Third, you establish the connection between the back end and the front end
             console.log('Front-end and back-end are connected');
         
             changeStream.on('change', (change) => {
