188325ec4afab0002985ef5c6074944294f3653b
abelmuro <abelmuro93@gmail.com>
2025-03-30 16:18:44 -0700

    commit

---
+++ src/Routes/DELETE/DeleteWebsockets.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+router.delete('/delete_websockets', (req, res) => {
+
+    try{
+        const websockets = global.webSocketHandlers;
+
+        for(let wss of websockets){
+            wss.clients.forEach((client) => {
+                if(client.ready.state === WebSocket.OPEN)
+                    client.close();
+            })  
+
+            wss.close((error) => {
+                if(error)
+                    console.log('error closing websocket', error);
+                else 
+                    console.log('Websocket server closed successfully')
+            })
+        }     
+        
+        res.status(200).send('All websocket servers have closed successfully');   
+    }
+    catch(error){
+        const message = error.message;
+        console.log(message);
+        res.status(500).send(message);
+    }
+
+})
