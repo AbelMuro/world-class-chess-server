55448f72c0244002bf3ba2e8314303e1f64c12a6
abelmuro <abelmuro93@gmail.com>
2025-04-05 14:05:30 -0700

    commit

--- src/Config/MongoDB/Models/Challenges.js
+++ src/Config/MongoDB/Models/Challenges.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const {Schema} = require('mongoose');
+const objectId = mongoose.Types.ObjectId;
 
 const challengesSchema = new Schema({
     playerOne: {type: String},
     playerTwo: {type: String},
     playerOneAccepted: {type: String},
     playerTwoAccepted: {type: String},
-    matchId: {type: String}
+    matchId: {type: objectId}
 });
 
 const Challenge = mongoose.model('challenge', challengesSchema, 'challenges');
