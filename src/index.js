5a35d02c61a779645d9fa15a5befb357358503ea
abelmuro <abelmuro93@gmail.com>
2025-04-11 10:47:27 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ const HTTPS_PORT = 443
 
 app.use(express.json());
 app.use(cookieParser());
+app.options('*', cors());
 app.use(cors({
     origin: ['https://world-class-chess.netlify.app', 'http://localhost:3000'],						//Access-Control-Allow-Origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
