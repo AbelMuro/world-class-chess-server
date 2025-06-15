7cd540463146c2914d44735418b831033e2bfcef
abelmuro <abelmuro93@gmail.com>
2025-06-15 10:45:26 -0700

    commit

--- src/Routes/POST/GuestLogin.js
+++ src/Routes/POST/GuestLogin.js
@@ -5,6 +5,7 @@ const {config} = require('dotenv');
 config();
 
 router.post('/guestlogin', async (req, res) => {
+    const JWT_SECRET = process.env.JWT_SECRET;
     let username = 'Guest'
     const guestId = Array.from({length: 10}, () => null).reduce((acc) => {acc += Math.floor(Math.random() * 9); return acc}, '');
     username += guestId;
