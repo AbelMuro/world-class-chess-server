20b95f7d6ddc41e1437ebe794f9a6f30e4b5b7a2
abelmuro <abelmuro93@gmail.com>
2025-09-16 08:55:52 -0700

    commit

--- functions/app.js
+++ functions/app.js
@@ -1,5 +1,11 @@
 const serverless = require('serverless-http'); 
-const app = require('../src/index.js'); 			//make sure you export the app module from the index.js
+const app = require('../src/index.js'); 		
+const connectDB = require('../src/Config/MongoDB/DB.js');
 
-const handler = serverless(app);  		      
-module.exports.handler = handler;
+const handler = serverless(app); 
+
+module.exports.handler = async (e, context) => {
+    await connectDB();
+    const result = await handler(e, context);
+    return result;
+};
